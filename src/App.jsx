525c43977441adf5e32d62473bd65d80a0e88576
“Nitish-2000” <nitishmani111@gmail.com>
2024-02-12 21:10:46 +0530

    BITSS

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path ="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
